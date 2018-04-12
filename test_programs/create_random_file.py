3430a64388ed6b9ccca713044d5e496e77450739
Hearues Zueke <hziko314@gmail.com>
2018-04-12 18:40:05 +0200

    something new too

---
+++ test_programs/create_random_file.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python2.7
+
+import numpy as np
+
+import os
+
+
+import sys
+sys.path.append('..')
+# sys.path.append('../encryption')
+
+from encryption import Utils
+
+arr_rnd = np.random.randint(0, 256, (1024*1024*1, )).astype(np.uint8)
+# print("arr_rnd:")
+# Utils.pretty_block_printer(arr_rnd, 8, len(arr_rnd))
+
+# path = "/home/doublepmcl/Document"
+# os.chdir(path)
+
+with open("/home/doublepmcl/Documents/random_data.hex", "wb") as fout:
+    arr_rnd.tofile(fout)
