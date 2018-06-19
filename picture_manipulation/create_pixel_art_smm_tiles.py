0bdd97e60f6aff22363d4bb4e0e945c20c4e01ea
Hearues Zueke <hziko314@gmail.com>
2018-06-19 03:21:26 +0200

    add more lines of code

--- picture_manipulation/create_pixel_art_smm_tiles.py
+++ picture_manipulation/create_pixel_art_smm_tiles.py
@@ -7,7 +7,43 @@ import os
 import numpy as np
 
 from PIL import Image
+
+"""
+    @param pix: is a numpy array with shape (x, y, z) (x%tw == 0, y%tw == 0)
+    @param tw: tile width (also the tile height)
+
+    @return: will return a numpy array with each square tiles separate
+"""
+def get_square_tiles(pix, tw):
+    x, y, z = pix.shape
+    return pix.transpose(0, 2, 1) \
+              .reshape((x//tw, tw*z, y)) \
+              .transpose(0, 2, 1) \
+              .reshape((x*y//tw//tw, tw, tw, z)) \
+              .transpose(0, 2, 1, 3) \
+              .reshape((x//tw, y//tw, tw, tw, z)) # this is crazy!
+
+def transform_square_tiles_to_img(pix, tw):
+    a, b, c, d, e = pix.shape
+    return pix.reshape((a*b, c, d, e)) \
+              .transpose(0, 2, 1, 3) \
+              .reshape((a, c*e, b*d)) \
+              .transpose(0, 2, 1) \
+              .reshape((a, b*d, c*e)) \
+              .transpose(0, 2, 1) \
+
+def get_approx_tiles(pix_img_tiles, tiles_row):
+    shape = pix_img_tiles.shape
+    print("pix_img_tiles.shape: {}".format(pix_img_tiles.shape))
+    pix_img_tiles = pix_img_tiles.reshape((shape[:2]+(1, )+shape[2:])).astype(np.int64)
+    print("pix_img_tiles.shape: {}".format(pix_img_tiles.shape))
+    idx_tbl = np.argmin(np.sum(np.sum(np.sum((pix_img_tiles-tiles_row)**2, axis=-1), axis=-1), axis=-1), axis=-1)
     
+    new_pix = tiles_row[idx_tbl]
+    print("new_pix.shape: {}".format(new_pix.shape))
+
+    return new_pix
+
 if __name__ == "__main__":
     from os.path import expanduser
     home = expanduser("~")
@@ -17,10 +53,10 @@ if __name__ == "__main__":
 
     img = Image.open(path_images+"smm_smb3_tiles.png")
     pix = np.array(img)
-
+    
     tw = 16 # tile width
     th = 16 # tile height
-    tiles_x = 4
+    tiles_x = 7
     tiles_y = 3
     pix_tiles = np.zeros((th*tiles_y, tw*tiles_x, 3), dtype=np.uint8)
 
@@ -36,18 +72,28 @@ if __name__ == "__main__":
                     [0, 33, 2, 0],
                     [0, 34, 2, 1],
                     [0, 36, 2, 2],
-                    [5, 38, 2, 3]]
+                    [5, 38, 2, 3],
 
+                    [0, 6, 0, 4],
+                    [6, 6, 0, 5],
+                    [7, 8, 0, 6],
+
+                    [0, 6+16, 1, 4],
+                    [6, 6+16, 1, 5],
+                    [7, 8+16, 1, 6],
+
+                    [0, 6+32, 2, 4],
+                    [6, 6+32, 2, 5],
+                    [7, 8+32, 2, 6]]
+    
+    # Fill the tiles into the pix_tiles
     for ts_y, ts_x, t_y, t_x in choose_tiles:
         pix_tiles[th*t_y:th*(t_y+1), tw*t_x:tw*(t_x+1)] = \
             pix[(th+1)*ts_y+1:(th+1)*ts_y+th+1, (tw+1)*ts_x+1:(tw+1)*ts_x+tw+1]
 
     img_tiles = Image.fromarray(pix_tiles)
-    img_tiles.show()
-
     img_anime = Image.open(path_images+"anime_1.jpeg")
-    img_anime.show()
-
+    
     pix_anime = np.array(img_anime)
     print("pix_anime.shape: {}".format(pix_anime.shape))
     anim_h, anim_w, anim_c = pix_anime.shape
@@ -60,11 +106,53 @@ if __name__ == "__main__":
     x, y, z = pix_tiles.shape
     print("pix_tiles.shape: {}".format(pix_tiles.shape))
 
-    pix_tiles_own = pix_tiles.transpose(0, 2, 1)
-    pix_tiles_own = pix_tiles_own.reshape((th, z*x//th, y))
-    pix_tiles_own = pix_tiles_own.transpose(0, 2, 1)
-    pix_tiles_own = pix_tiles_own.reshape((x*y//th//tw, th, tw, z))
-    pix_tiles_own = pix_tiles_own.transpose(0, 2, 1, 3)
-    pix_tiles_own = pix_tiles_own.reshape((x//th, y//tw, th, tw, z))
+    # TODO: Fix this! (maybe)
+    pix_tiles_own = get_square_tiles(pix_tiles, tw)
+    pix_anime_own = get_square_tiles(pix_anime, tw)
 
     print("pix_tiles_own.shape: {}".format(pix_tiles_own.shape))
+    print("pix_anime_own.shape: {}".format(pix_anime_own.shape))
+
+    # pix_tiles_own = pix_tiles.transpose(0, 2, 1)
+    # pix_tiles_own = pix_tiles_own.reshape((x//th, z*th, y)) # watch out the fail next time!
+    # pix_tiles_own = pix_tiles_own.transpose(0, 2, 1)
+    # pix_tiles_own = pix_tiles_own.reshape((x*y//th//tw, th, tw, z))
+    # pix_tiles_own = pix_tiles_own.transpose(0, 2, 1, 3)
+    # pix_tiles_own = pix_tiles_own.reshape((x//th, y//tw, th, tw, z))
+    
+    # pix_tiles_own = pix_tiles_own.reshape((x*y//tw, tw, z))
+
+    # print("pix_tiles_own.shape: {}".format(pix_tiles_own.shape))
+    # img_tiles_own = Image.fromarray(pix_tiles_own)
+    # img_tiles_own.show()
+
+    first_row = pix_tiles_own[0]
+    # print("first_row.shape: {}".format(first_row.shape))
+
+    # # TODO: add new snippet later
+    # # print("first_row_col: "+f"{first_row_col}")
+
+    # first_row_col = first_row.reshape((tiles_x*th, tw, 3))
+    # img_row_col = Image.fromarray(first_row_col)
+
+    # print("first_row_col.shape: {}".format(first_row_col.shape))
+    # img_row_col.show()
+
+
+    img_tiles.show()
+    # img_anime.show()
+
+    # pix_approx = pix_anime.copy()
+    # pix_approx_tiles = get_square_tiles(pix_approx, tw)
+
+    # Use this as a link (another reference!)
+    # pix_approx_tiles[:] = get_approx_tiles(pix_anime_own, first_row).astype(np.uint8)
+    pix_approx_tiles = get_approx_tiles(pix_anime_own, first_row).astype(np.uint8)
+    pix_approx = transform_square_tiles_to_img(pix_approx_tiles, tw)
+    print("pix_approx.shape: {}".format(pix_approx.shape))
+    print("pix_approx.dtype: {}".format(pix_approx.dtype))
+    # TODO: need to fix this!!!
+    # img_approx = Image.fromarray(pix_approx_tiles[:2].reshape((2*16, 16*14, 3)))
+    img_approx = Image.fromarray(pix_approx)
+
+    img_approx.show()
