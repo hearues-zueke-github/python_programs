58870930bf8f4a546b5032030ca82075e34ddaff
Hearues Zueke <hziko314@gmail.com>
2018-07-21 05:39:30 +0200

    faster approach for finding repeat pattern

--- math_numbers/find_repeat_decimal_pattern.py
+++ math_numbers/find_repeat_decimal_pattern.py
@@ -37,24 +37,6 @@ def find_repeats_length(str_num):
     return "-", 0
 
 
-def find_beginning_index_pattern(str_num, repeat):
-    start_idx = str_num.find(repeat)
-    if start_idx <= 0:
-        return start_idx, repeat
-
-    length = len(repeat)
-
-    # print("start_idx: {}".format(start_idx))
-    temp_pattern = str(repeat)
-    for i in range(start_idx-1, -1, -1):
-        temp2_pattern = temp_pattern[-1]+temp_pattern[:-1]
-        if str_num[i:i+length] != temp2_pattern:
-            break
-        temp_pattern = temp2_pattern
-
-    return i, temp_pattern
-
-
 if __name__ == "__main__":
     from os.path import expanduser
     home = expanduser("~")
@@ -74,56 +56,58 @@ if __name__ == "__main__":
     decimal.getcontext().prec = precision
 
     repeat_num_lens = []
+    pattern_lengths = []
     mult_factor = []
-    # start_idxs = []
 
     dec_1 = Dec(1)
-    for p in ps[3:2000]:
+    for p in ps[3:8000]:
         decimal.getcontext().prec = int(p)+4
 
-        # dec_p = Dec(int(p))
-        # num = dec_1 / dec_p
-
         num = dec_1 / Dec(int(p))
-        # print("num: {}".format(num))
 
         str_num = str(num)[2:]
-        # print("p: {}".format(p))
-        # print("p: {}, str_num: {}".format(p, str_num))
-
-        # print("type(str_num): {}".format(type(str_num)))
-        # print("p: {}, len(str_num): {}".format(p, len(str_num)))
-
-        # print("str_num[::-1]: {}".format(str_num[::-1]))
-
-        # repeat_num, repeat_length = find_repeats_length(str_num[::-1])
-        # repeat_num = repeat_num[::-1]
         
         repeat_num, repeat_length = find_repeats_length(str_num)
-
-        # print("repeat_num: {}, repeat_length: {}".format(repeat_num, repeat_length))
-
-        # start_idx, true_pattern = find_beginning_index_pattern(str_num, repeat_num)
-        # print("start_idx: {}, true_pattern: {}".format(start_idx, true_pattern))
 
         if repeat_num == "-":
             l_rep = p-1
         else:
             l_rep = len(repeat_num)
 
+        if l_rep == 32:
+            print("l_rep: 32, p: {}".format(p))
+
         factor = (p-1)//l_rep
         repeat_num_lens.append((p, l_rep, factor))
+        pattern_lengths.append(l_rep)
         mult_factor.append(factor)
-        # start_idxs.append(start_idx)
+
+    pattern_lengths = np.array(pattern_lengths)
+    unique_pattern_lengths = np.array(sorted(list(set(pattern_lengths))))
+    pattern_lengths_hist_nums = np.sum(pattern_lengths == unique_pattern_lengths.reshape((-1, 1)), axis=-1)
+    # use only lengths, which comes more then once!
+    print("before: len(unique_pattern_lengths): {}".format(len(unique_pattern_lengths)))
+    choose_nums = pattern_lengths_hist_nums > 1
+
+    unique_pattern_lengths = unique_pattern_lengths[choose_nums]
+    pattern_lengths_hist_nums = pattern_lengths_hist_nums[choose_nums]
+    print("after: len(unique_pattern_lengths): {}".format(len(unique_pattern_lengths)))
+
+    pattern_lengths_hist = [(length, amount) for length, amount in zip(unique_pattern_lengths, pattern_lengths_hist_nums)]
 
     mult_factor = np.array(mult_factor)
-    unique_mult_factor = sorted(list(set(mult_factor)))
-    mult_factor_hist_nums = np.sum(mult_factor==np.array(unique_mult_factor).reshape((-1, 1)), axis=-1)
-    mult_factor_hist = {factor: factor_hist for factor, factor_hist in zip(unique_mult_factor, mult_factor_hist_nums)}
+    unique_mult_factor = np.array(sorted(list(set(mult_factor))))
+    mult_factor_hist_nums = np.sum(mult_factor == unique_mult_factor.reshape((-1, 1)), axis=-1)
+    mult_factor_hist = [(factor, factor_hist) for factor, factor_hist in zip(unique_mult_factor, mult_factor_hist_nums)]
 
-    print("repeat_num_lens: {}".format(repeat_num_lens))
+    # print("repeat_num_lens: {}".format(repeat_num_lens))
     print("mult_factor: {}".format(mult_factor))
     print("mult_factor_hist: {}".format(mult_factor_hist))
-    print("unique_mult_factor: {}".format(unique_mult_factor))
-    print("mult_factor_hist_nums: {}".format(mult_factor_hist_nums))
+    # print("unique_mult_factor: {}".format(unique_mult_factor))
+    # print("mult_factor_hist_nums: {}".format(mult_factor_hist_nums))
+
+    print("pattern_lengths: {}".format(pattern_lengths))
+    print("pattern_lengths_hist: {}".format(pattern_lengths_hist))
+    # print("unique_pattern_lengths: {}".format(unique_pattern_lengths))
+    # print("pattern_lengths_hist_nums: {}".format(pattern_lengths_hist_nums))
     # print("start_idxs: {}".format(start_idxs))
