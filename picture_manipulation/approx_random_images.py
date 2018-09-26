21d82687703b3836f735ad3ea978b5016a1f94fc
Hearues Zueke <hziko314@gmail.com>
2018-09-26 08:18:32 +0200

    add new stuff too

--- picture_manipulation/approx_random_images.py
+++ picture_manipulation/approx_random_images.py
@@ -2,16 +2,22 @@
 
 import os
 import pdb
+import string
 import sys
 
 import numpy as np
 
 from PIL import Image
 
+def get_random_64_bit_number(n):
+    s = string.ascii_lowercase+string.ascii_uppercase+string.digits+"-_"
+    arr = np.array(list(s))
+    return "".join(np.random.choice(arr, (n, )).tolist())
+
 def apply_neighbour_logic(pix_bw, choosen_algo=0):
-    # # add the frame to the image with itself
-    # # e.g. the left 2 cols add to the right side, and vice versa for all other sides
-    # pix_bw = (lambda x: np.hstack((x[:, -2:], x, x[:, :2])))(np.vstack((pix_bw[-2:], pix_bw, pix_bw[:2])).copy())
+    # add the frame to the image with itself
+    # e.g. the left 2 cols add to the right side, and vice versa for all other sides
+    pix_bw = (lambda x: np.hstack((x[:, -2:], x, x[:, :2])))(np.vstack((pix_bw[-2:], pix_bw, pix_bw[:2])).copy())
     
     height, width = pix_bw.shape[:2]
 
@@ -72,7 +78,7 @@ def apply_neighbour_logic(pix_bw, choosen_algo=0):
         exec("globals()['{}'] = pixs[{}, {}]".format(value, key[0], key[1]))
 
     p = pix_bw
-    idx_algo = [1, 3, 4, 2, 5]
+    idx_algo = [1, 3, 4, 2, 11, 7, 9]
     fs = [
         lambda: \
         ((p_uu&p_dd&p_rr&p_ll|p_u&p_d&p_l&p_r)&(p==0))|
@@ -220,9 +226,9 @@ def apply_neighbour_logic(pix_bw, choosen_algo=0):
     pix_bw_3 = fs[idx_algo[choosen_algo+2]]()
     pix_bw = (pix_bw_1^pix_bw_2^pix_bw_3).astype(np.uint8)
 
-    # # remove the frame from the image again
-    # return pix_bw[2:-2, 2:-2]
-    return pix_bw
+    # remove the frame from the image again
+    return pix_bw[2:-2, 2:-2]
+    # return pix_bw
 
 def create_1_bit_neighbour_pictures(height, width):
     path_pictures = "images/changing_bw_1_bit_{}_{}/".format(height, width)
@@ -279,20 +285,44 @@ def create_1_byte_neighbour_pictures(height, width):
         Image.fromarray(pix_combine).save(path_pictures+"rnd_{}_{}_bw_iter_{:03}.png".format(height, width, it))
         it += 1
 
-def create_3_byte_neighbour_pictures(height, width):
-    path_pictures = "images/changing_bw_3_byte_{}_{}/".format(height, width)
+def create_3_byte_neighbour_pictures(img_type, f_args):
+    if img_type == "picture":
+        image_path = f_args[0]
+
+        if not os.path.exists(image_path):
+            print("Path to image '{}' does not exists!".format(image_path))
+            return -1
+
+        img = Image.open(image_path)
+        pix_img = np.array(img)
+        height, width = pix_img.shape[:2]
+
+        path_pictures = "images/changing_image_{}_{}/".format(height, width)
+        
+        pix_bws = [(pix_c>>j)&0x1 for pix_c in [pix_img[:, :, i] for i in range(0, 3)] for j in range(0, 8)]
+
+    elif img_type == "random":
+        height = f_args[0]
+        width = f_args[1]
+
+        path_pictures = "images/changing_bw_3_byte_{}_{}/".format(height, width)
+    
+        get_pix_bw = lambda: np.random.randint(0, 2, (height, width), dtype=np.uint8)
+        pix_bws = [get_pix_bw() for _ in range(0, 24)]
     
     if os.path.exists(path_pictures):
         os.system("rm -rf {}".format(path_pictures))
     if not os.path.exists(path_pictures):
         os.makedirs(path_pictures)
     
+    path_animations = "images/animations/"
+    path_movies = "images/movies/"
+    if not os.path.exists(path_animations):
+        os.makedirs(path_animations)
+    if not os.path.exists(path_movies):
+        os.makedirs(path_movies)
+    
     prev_folder = os.getcwd()
-    # os.chdir("./{}".format(path_pictures))
-    # os.chdir(prev_folder)
-
-    get_pix_bw = lambda: np.random.randint(0, 2, (height, width), dtype=np.uint8)
-    pix_bws = [get_pix_bw() for _ in range(0, 24)]
 
     def combine_1_byte_neighbours(pix_bws):
         def combine_1_bit_neighbours(pix_bws):
@@ -309,20 +339,19 @@ def create_3_byte_neighbour_pictures(height, width):
 
     pix_combine = combine_1_byte_neighbours(pix_bws)
     pix_combines = [pix_combine]
-    # Image.fromarray(pix_combine).save(path_pictures+"rnd_{}_{}_bw_iter_{:03}.png".format(height, width, 0))
 
     # so long there are white pixels, repeat the elimination_process!
     it = 1
     # repeat anything until it is complete blank / black / 0
     while np.sum([np.sum(pix_bw == 1) for pix_bw in pix_bws]) > 0:
-        print("it: {}".format(it))
+        if it%10 == 0:
+            print("it: {}".format(it))
         
         for i in range(0, 24):
-            pix_bws[i] = apply_neighbour_logic(pix_bws[i], choosen_algo=it%2)
+            pix_bws[i] = apply_neighbour_logic(pix_bws[i], choosen_algo=it%4)
 
         pix_combine = combine_1_byte_neighbours(pix_bws)
         pix_combines.append(pix_combine)
-        # Image.fromarray(pix_combine).save(path_pictures+"rnd_{}_{}_bw_iter_{:03}.png".format(height, width, it))
         it += 1
 
     # now take each image and interpolate between each image e.g. 10 samples
@@ -332,167 +361,47 @@ def create_3_byte_neighbour_pictures(height, width):
     path_template = path_pictures+"rnd_{}_{}_bw_i_{{:03}}_{{:02}}.png".format(height, width)
     for i, (pix_1, pix_2) in enumerate(zip(pix_combines[:-1], pix_combines[1:])):
         Image.fromarray(pix_1).save(path_template.format(i, 0))
-        amount_combines = 1
+        amount_combines = 5
         for j in range(1, amount_combines):
             print("i: {}, j: {}".format(i, j))
             Image.fromarray(get_pix_between(pix_1, pix_2, float(amount_combines-j)/amount_combines)).save(path_template.format(i, j))
 
     Image.fromarray(arr_pix_combines[-1]).save(path_template.format(arr_pix_combines.shape[0]-1, 0))
 
-    # os.system("ls")
     os.chdir("./{}".format(path_pictures))
 
-    for root_dir, dirs, files in os.walk("."):
-        # print("root_dir: {}".format(root_dir))
-        # print("files: {}".format(files))
-        # print("dirs: {}".format(dirs))
-        if not root_dir == ".":
-            continue
-
-        for file_name in files:
-            if not ".png" in file_name:
-                print("continue: file_name: {}".format(file_name))
+    if img_type == "random":
+        for root_dir, dirs, files in os.walk("."):
+            if not root_dir == ".":
                 continue
-            print("Resize, convert and reduce quality for file: '{}'".format(file_name))
-            os.system("convert {} -filter Point -resize 256x256 +antialias {}".format(file_name, file_name))
-            # os.system("mogrify -format jpg {}".format(file_name))
-            # file_name_jpg = file_name.replace(".png", ".jpg")
-            # os.system("convert {} -quality 20% {}".format(file_name_jpg, file_name_jpg))
+
+            for file_name in files:
+                if not ".png" in file_name:
+                    print("continue: file_name: {}".format(file_name))
+                    continue
+                print("Resize, convert and reduce quality for file: '{}'".format(file_name))
+                os.system("convert {} -filter Point -resize 256x256 +antialias {}".format(file_name, file_name))
     
     for root_dir, dirs, files in os.walk("."):
         if not root_dir == ".":
             continue
 
         arr = np.sort(np.array(files))
-        for i, file_name in enumerate(arr):
-            os.system("mv {} pic_{:04d}.png".format(file_name, i))
+        file_num = 0
+        for file_name in arr:
+            if file_num == 0:
+                for _ in range(0, 3):
+                    os.system("cp {} pic_{:04d}.png".format(file_name, file_num))
+                    file_num += 1
+            os.system("mv {} pic_{:04d}.png".format(file_name, file_num))
+            file_num += 1
 
-    # print("Create an animation (gif) with png's!")
-    # os.system("convert -delay 5 -loop 0 *.png animated_png.gif")
-    print("Create an animation (mp4) with png's!")
-    os.system("ffmpeg -r 20 -i pic_%04d.png -vcodec mpeg4 -y movie.mp4")
-
-    os.chdir(prev_folder)
-
-def create_from_image_neighbour_pictures(image_path):
-    if not os.path.exists(image_path):
-        print("Path to image '{}' does not exists!".format(image_path))
-        sys.exit(-1)
-
-    img = Image.open(image_path)
-    # img.show()
-    pix_img = np.array(img)
-    height, width = pix_img.shape[:2]
-    # print("height: {}, width: {}".format(height, width))
-    # print("height//2: {}, width//2: {}".format(height//2, width//2))
-    # print("height//4: {}, width//4: {}".format(height//4, width//4))
-    # sys.exit(0)
-
-    path_pictures = "images/changing_image_{}_{}/".format(height, width)
-    
-    if os.path.exists(path_pictures):
-        os.system("rm -rf {}".format(path_pictures))
-    if not os.path.exists(path_pictures):
-        os.makedirs(path_pictures)
-    
-    prev_folder = os.getcwd()
-    # os.chdir("./{}".format(path_pictures))
-
-    # get_pix_bw = lambda: np.random.randint(0, 2, (height, width), dtype=np.uint8)
-    # pix_bws = [get_pix_bw() for _ in range(0, 24)]
-    pix_bws = [(pix_c>>j)&0x1 for pix_c in [pix_img[:, :, i] for i in range(0, 3)] for j in range(0, 8)]
-
-    def combine_1_byte_neighbours(pix_bws):
-        def combine_1_bit_neighbours(pix_bws):
-            pix = np.zeros(pix_bws[0].shape, dtype=np.uint8)
-            for i, pix_bw in enumerate(pix_bws):
-                pix += pix_bw<<i
-            return pix
-
-        pix_bw_channels = [combine_1_bit_neighbours(pix_bws[8*i:8*(i+1)]) for i in range(0, 3)]
-        pix = np.zeros(pix_bw_channels[0].shape+(3, ), dtype=np.uint8)
-        for i, pix_bw_c in enumerate(pix_bw_channels):
-            pix[:, :, i] = pix_bw_c
-        return pix
-
-    pix_combine = combine_1_byte_neighbours(pix_bws)
-    # img_combine = Image.fromarray(pix_combine)
-    # img_combine.show()
-    # print("pix_img.shape: {}".format(pix_img.shape))
-    # print("pix_combine.shape: {}".format(pix_combine.shape))
-    # not_equal_bytes = np.sum(pix_img!=pix_combine)
-    # print("not_equal_bytes: {}".format(not_equal_bytes))
-    # Image.fromarray(pix_img).show()
-    # sys.exit(0)
-    pix_combines = [pix_combine]
-    # Image.fromarray(pix_combine).save(path_pictures+"rnd_{}_{}_bw_iter_{:03}.png".format(height, width, 0))
-
-    # so long there are white pixels, repeat the elimination_process!
-    it = 1
-    # repeat anything until it is complete blank / black / 0
-    while np.sum([np.sum(pix_bw == 1) for pix_bw in pix_bws]) > 0:
-        print("it: {}".format(it))
-        
-        for i in range(0, 24):
-            pix_bws[i] = apply_neighbour_logic(pix_bws[i], choosen_algo=(it)%2)
-
-        pix_combine = combine_1_byte_neighbours(pix_bws)
-        pix_combines.append(pix_combine)
-        # Image.fromarray(pix_combine).save(path_pictures+"rnd_{}_{}_bw_iter_{:03}.png".format(height, width, it))
-        it += 1
-
-    # now take each image and interpolate between each image e.g. 10 samples
-    def get_pix_between(pix_1, pix_2, alpha=0.5):
-        return (pix_1.astype(np.float)*alpha+pix_2.astype(np.float)*(1.-alpha)).astype(np.uint8)
-    arr_pix_combines = np.array(pix_combines)
-    path_template = path_pictures+"rnd_{}_{}_bw_i_{{:03}}_{{:02}}.png".format(height, width)
-    for i, (pix_1, pix_2) in enumerate(zip(pix_combines[:-1], pix_combines[1:])):
-        Image.fromarray(pix_1).save(path_template.format(i, 0))
-        amount_combines = 2
-        for j in range(1, amount_combines):
-            print("i: {}, j: {}".format(i, j))
-            Image.fromarray(get_pix_between(pix_1, pix_2, float(amount_combines-j)/amount_combines)).save(path_template.format(i, j))
-
-    Image.fromarray(arr_pix_combines[-1]).save(path_template.format(arr_pix_combines.shape[0]-1, 0))
-
-    # os.system("ls")
-    os.chdir("./{}".format(path_pictures))
-
-    for root_dir, dirs, files in os.walk("."):
-        # print("root_dir: {}".format(root_dir))
-        # print("files: {}".format(files))
-        # print("dirs: {}".format(dirs))
-        if not root_dir == ".":
-            continue
-
-        for file_name in files:
-            if not ".png" in file_name:
-                print("continue: file_name: {}".format(file_name))
-                continue
-            print("Resize, convert and reduce quality for file: '{}'".format(file_name))
-            # os.system("convert {} -filter Point -resize 256x256 +antialias {}".format(file_name, file_name))
-            # os.system("mogrify -format jpg {}".format(file_name))
-            # file_name_jpg = file_name.replace(".png", ".jpg")
-            # os.system("convert {} -quality 20% {}".format(file_name_jpg, file_name_jpg))
-    
-    
-    # print("Create an animation with jpg's!")
-    # os.system("convert -delay 2 -loop 0 *.jpg animated_jpg.gif")
-    
-    for root_dir, dirs, files in os.walk("."):
-        if not root_dir == ".":
-            continue
-
-        arr = np.sort(np.array(files))
-        for i, file_name in enumerate(arr):
-            os.system("mv {} pic_{:04d}.png".format(file_name, i))
-
-    # pdb.set_trace()
-
-    # print("Create an animation (gif) with png's!")
-    # os.system("convert -delay 5 -loop 0 *.png animated_png.gif")
-    print("Create an animation (mp4) with png's!")
-    os.system("ffmpeg -r 20 -i pic_%04d.png -vcodec mpeg4 -y movie.mp4")
+    random_64_bit_num = get_random_64_bit_number(4)
+    suffix = "_{}_{}_{}_{}".format(img_type, height, width, random_64_bit_num)
+    print("Create an animation (gif) with png's and suffix '{}'!".format(suffix))
+    os.system("convert -delay 5 -loop 0 *.png ../../{}animated{}.gif".format(path_animations, suffix))
+    print("Create an animation (mp4) with png's and suffix '{}'!".format(suffix))
+    os.system("ffmpeg -r 20 -i pic_%04d.png -vcodec mpeg4 -y ../../{}movie{}.mp4".format(path_movies, suffix))
 
     os.chdir(prev_folder)
 
@@ -505,7 +414,7 @@ if __name__ == "__main__":
 
     # create_1_bit_neighbour_pictures(height, width)
     # create_1_byte_neighbour_pictures(height, width)
-    create_3_byte_neighbour_pictures(height, width)    
+    create_3_byte_neighbour_pictures("random", (height, width))
     # create_from_image_neighbour_pictures("images/fall-autumn-red-season.jpg")
     # ## convert fall-autumn-red-season.jpg -resize 320x213 fall-autumn-red-season_resized.jpg
-    create_from_image_neighbour_pictures("images/fall-autumn-red-season_resized.jpg")
+    create_3_byte_neighbour_pictures("picture", ("images/fall-autumn-red-season_resized.jpg", ))
