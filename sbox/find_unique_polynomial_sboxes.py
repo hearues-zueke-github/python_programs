657be76f9dcd5cb677887df60a34014957788006
Hearues Zueke <hziko314@gmail.com>
2018-10-11 15:21:27 +0200

    found the major buggit add find_unique_polynomial_sboxes.py !

--- sbox/find_unique_polynomial_sboxes.py
+++ sbox/find_unique_polynomial_sboxes.py
@@ -19,34 +19,30 @@ def apply_polynomial_function(n, p, factors):
     assert isinstance(n, int)
     assert n > 0
     assert isinstance(p, int)
-    # assert isinstance(factors, list)
-    # assert len(factors) == p+1
     assert factors.shape[1] == p+1
 
+    factors = factors.T
     x = np.arange(0, n, dtype=object)
-
-    # TODO: need to finish this up!
-    f_x = np.zeros((factors.shape[0], n), dtype=object)+factors[:, 0].reshape((-1, 1))
-    for i, a_i in zip(range(1, p+1), factors.T[1:]):
+    f_x = np.zeros((factors.shape[1], n), dtype=object)+factors[0].reshape((-1, 1))
+    for i, a_i in zip(range(1, p+1), factors[1:]):
         f_x += a_i.reshape((-1, 1))*x**i
     
     return f_x % n
 
 if __name__ == "__main__":
-    # ns = 40 # amount of elements = modulo
-    ps = 2 # max power
+    ns = list(2**i for i in range(0, 5))
+    ps = 3 # max power
     # max_p = 1
     # arr = np.arange(0, n, dtype=object) # object is used bcs of BigInt of python
-
-    lens_f_x = {}
-    # rows_n = list(range(3, ns+1))
-    ns = list(2**i for i in range(0, 8))
     
-    lens_arr = np.zeros((len(ns)+1, ps+1), dtype=np.int)
+    lens_max_arr = np.zeros((len(ns)+1, ps+1), dtype=np.int)
     factors_arr = np.zeros((len(ns)+1, ps+1), dtype=object)
     
-    lens_arr[1:, 0] = ns
-    lens_arr[0] = np.arange(0, ps+1)
+    print("lens_max_arr.shape: {}".format(lens_max_arr.shape))
+    print("factors_arr.shape: {}".format(factors_arr.shape))
+
+    lens_max_arr[1:, 0] = ns
+    lens_max_arr[0] = np.arange(0, ps+1)
     factors_arr[1:, 0] = ns
     factors_arr[0] = np.arange(0, ps+1)
     
@@ -55,35 +51,47 @@ if __name__ == "__main__":
      # print("n: {}".format(n))
      for p in range(1, ps+1):
         lens = set()
-        unique_factors = set()
-        unique_f_x = {}
         unique_f_x_only = set()
-        
-        # if p == 1:
-        #     factors = np.zeros((n, n, 2), dtype=np.uint8)
-        #     factors[:, :, 0] = np.arange(0, n)
-        #     factors[:, :, 1] = np.arange(0, n).reshape((-1, 1))
-        # elif p == 2:
-        #     factors = np.zeros((n, n, n, 3), dtype=np.uint8)
-        #     factors[:, :, :, 0] = np.arange(0, n)
-        #     factors[:, :, :, 1] = np.arange(0, n).reshape((-1, 1))
-        #     factors[:, :, :, 2] = np.arange(0, n).reshape((-1, 1, 1))
-        # elif p == 3:
-        #     factors = np.zeros((n, n, n, n, 4), dtype=np.uint8)
-        #     factors[:, :, :, :, 0] = np.arange(0, n)
-        #     factors[:, :, :, :, 1] = np.arange(0, n).reshape((-1, 1))
-        #     factors[:, :, :, :, 2] = np.arange(0, n).reshape((-1, 1, 1))
-        #     factors[:, :, :, :, 3] = np.arange(0, n).reshape((-1, 1, 1, 1))
-        # factors = factors.reshape((-1, p+1))
 
         hit = 0
         miss = 0
-        try:
-            fs_x = apply_polynomial_function(n, p, factors)
-            print("\nfactors.shape: {}".format(factors.shape))
 
-            factors_sbox = []
-            for facts, row in zip(factors, fs_x):
+        factors = np.zeros((n, )*p+(p+1, ), dtype=np.int)
+        
+        exec_str_temp_before = "factors["+":"+(", :"*(p-1) if p > 1 else "")+", {}] = np.arange(0, n).reshape((-1{}))"
+        for i in range(1, p+1):
+            exec(exec_str_temp_before.format(i, (", 1"*(i-1) if i > 1 else ", ")))
+        
+        exec_str_temp = "factors["+":"+(", :"*(p-1) if p > 1 else "")+", 0] = {}"
+        
+        factors_sbox = []
+
+        # exec(exec_str_temp.format(0))
+        # facts_arr = factors.copy()
+        # fss_x = apply_polynomial_function(n, p, factors.reshape((-1, p+1)))
+
+        # for v in range(1, n):
+        #     exec(exec_str_temp.format(v))
+        #     facts_arr = np.vstack((facts_arr, factors))
+        #     fs_x = apply_polynomial_function(n, p, factors.reshape((-1, p+1)))
+        #     fss_x = np.vstack((fss_x, fs_x))
+
+        # facts_arr = facts_arr.reshape((-1, p+1))
+        # fss_x = fss_x.reshape((-1, n))
+
+        # print("facts_arr:\n{}".format(facts_arr))
+        # print("fss_x:\n{}".format(fss_x))
+        # input()
+            
+        # if True:
+        #     for facts, row in zip(facts_arr, fss_x):
+
+        for v in range(0, n):
+            exec(exec_str_temp.format(v))
+            fs_x = apply_polynomial_function(n, p, factors.reshape((-1, p+1)))
+
+            for facts, row in zip(factors.reshape((-1, p+1)), fs_x):
+            
                 unique_len = len(np.unique(row))
                 lens.add(unique_len)
 
@@ -92,40 +100,14 @@ if __name__ == "__main__":
                     f_x_tpl = tuple(row)
                     if not f_x_tpl in unique_f_x_only:
                         unique_f_x_only.add(f_x_tpl)
-                        factors_sbox.append(facts)
-                        # print("len(unique_f_x_only): {}".format(len(unique_f_x_only)))
-                    # if not factors_tpl in unique_factors:
-                    #     unique_factors.add(factors_tpl)
-                    #     print("factors: {}, len(unique_factors): {}".format(factors, len(unique_factors)))
-                    #     # print("unique_factors: {}".format(unique_factors))
-                    #     f_x_tpl = tuple(f_x)
-                    #     if f_x_tpl in unique_f_x:
-                    #         unique_f_x[f_x_tpl].add(factors_tpl)
-                    #     else:
-                    #         unique_f_x[f_x_tpl] = set([factors_tpl])
+                        factors_sbox.append(facts.tolist())
                 else:
                     miss += 1
 
-        except KeyboardInterrupt:
-            pass
-
-        # for f_x_num, f_x_tpl in enumerate(unique_f_x):
-        #     print("f_x_num: {}, f_x_tpl: {}".format(f_x_num, f_x_tpl))
-        #     all_factors = list(unique_f_x[f_x_tpl])
-        #     for i, factors in enumerate(all_factors):
-        #         print("  i: {}, factors: {}".format(i, factors))
-        # arr_factors = np.array(list(unique_factors), dtype=np.int64)
-        # sorted_factors  = np.sort(arr_factors.reshape((-1, )).view("u8"+(",u8"*p if p > 1 else ""))).view("u8").reshape(arr_factors.shape)
-        
-        print("n: {}, p: {}, len(unique_f_x_only): {}".format(n, p, len(unique_f_x_only)))
+        print("\nn: {}, p: {}, len(unique_f_x_only): {}".format(n, p, len(unique_f_x_only)))
         print("hit: {}, miss: {}".format(hit, miss))
         print("Chance of getting a unique f_x by random: {:02.3f}%".format(hit/float(hit+miss)*100))
         
-        # print("sorted_factors:\n{}".format(sorted_factors))
-
-        lens_f_x[(row_n, p)] = [len(unique_f_x_only), hit, miss]
-        lens_arr[row_n, p] = len(unique_f_x_only)
+        lens_max_arr[row_n, p] = len(unique_f_x_only)
+        # print("factors_sbox: {}".format(factors_sbox))
         factors_arr[row_n, p] = factors_sbox
-
-    keys = sorted(list(lens_f_x.keys()))
-
