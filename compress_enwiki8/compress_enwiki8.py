3a1e71565f6aed4b58b5ebd41d8dae2cbbe21cd1
Hearues Zueke <hziko314@gmail.com>
2020-04-12 07:09:11 +0200

    add more ideas for compressing enwik8

--- compress_enwiki8/compress_enwiki8.py
+++ compress_enwiki8/compress_enwiki8.py
@@ -7,11 +7,10 @@ import gzip
 import os
 import sys
 
-from dotmap import DotMap
-
-# from sortedcontainers import SortedSet
-
 from collections import defaultdict
+from dotmap import DotMap
+from operator import itemgetter
+# from sortedcontainers import SortedSet
 
 from os.path import expanduser
 PATH_HOME = expanduser("~")+'/'
@@ -59,22 +58,37 @@ def create_dict_word_count_for_arr(arr, max_byte_length=10):
 
 if __name__ == "__main__":
 
-    arr = utils_compress_enwik8.get_arr(used_length=2**16)
+    arr = utils_compress_enwik8.get_arr(used_length=2**19)
     # arr = utils_compress_enwik8.get_arr(used_length=2**23)
     bytes_starting_size = arr.shape[0]
     # arr = utils_compress_enwik8.get_arr(used_length=2**22+1)
     
-    OBJ_NAME_D_ARR_COMB = 'd_arr_comb_size_arr_{}'.format(arr.shape[0])
-    OBJ_NAME_D_ARR_COMB_UNIQUE = 'd_arr_comb_unique_size_arr_{}'.format(arr.shape[0])
 
     # global_object_getter_setter.delete_object(OBJ_NAME_D_ARR_COMB)
     # global_object_getter_setter.delete_object(OBJ_NAME_D_ARR_COMB_UNIQUE)
 
-    MAX_BYTE_LENGTH = 10
+
+    # LEN_CHOSEN_INDEX = 2**5
+    # LEN_BITS_CHOSEN_INDEX = len(bin(LEN_CHOSEN_INDEX-1))-2
+
+    argv = sys.argv
+    LEN_BITS_CHOSEN_INDEX = int(argv[1])
+    # LEN_BITS_CHOSEN_INDEX = 2
+    LEN_CHOSEN_INDEX = 2**LEN_BITS_CHOSEN_INDEX
+
+    MAX_BYTE_LENGTH = 16
+    OBJ_NAME_D_ARR_COMB = 'd_arr_comb_size_arr_{}_max_byte_length_{}'.format(arr.shape[0], MAX_BYTE_LENGTH)
+    OBJ_NAME_D_ARR_COMB_UNIQUE = 'd_arr_comb_unique_size_arr_{}_max_byte_length_{}'.format(arr.shape[0], MAX_BYTE_LENGTH)
+
+    ROUNDS_AMOUNT = 2
 
     l_content_bits_compressed = []
     l_arr_content_compressed_uint8 = []
-    for round_nr in range(0, 15):
+    l_len_raw_bits = []
+    l_len_compressed_bits = []
+    for round_nr in range(0, ROUNDS_AMOUNT):
+        print("round_nr: {}".format(round_nr))
+        round_nr = 0
         if round_nr==0:
             if not global_object_getter_setter.do_object_exist(OBJ_NAME_D_ARR_COMB) or \
               not global_object_getter_setter.do_object_exist(OBJ_NAME_D_ARR_COMB_UNIQUE):
@@ -88,96 +102,252 @@ if __name__ == "__main__":
         else:
             d_arr_comb, d_arr_comb_unique = create_dict_word_count_for_arr(arr, max_byte_length=MAX_BYTE_LENGTH)
 
-        l = []
-        for byte_amount in range(2, MAX_BYTE_LENGTH+1):
-            print("byte_amount: {}".format(byte_amount))
-            arr_comb = d_arr_comb[byte_amount]
-            for i_unique, unique_bytes in enumerate(d_arr_comb_unique[byte_amount]['u'][:20], 0):
-                print("i_unique: {}".format(i_unique))
-                idxs_nr = np.where(np.all(arr_comb==unique_bytes, axis=1))[0]
-                idxs_ranges = np.vstack((idxs_nr, idxs_nr+byte_amount)).T
-                l_idxs_ranges = utils_compress_enwik8.get_list_of_ranges(idxs_ranges)
-                l.append(l_idxs_ranges[0])
+        d_word_occurences = {}
+        d_word_weight = {}
+        for comb_nr in range(2, MAX_BYTE_LENGTH+1):
+            print("comb_nr: {}".format(comb_nr))
+            arr_comb = d_arr_comb[comb_nr]
+            arr_comb_unique = d_arr_comb_unique[comb_nr]
+            u = arr_comb_unique['u']
+            c = arr_comb_unique['c']
 
-        l_info = [((v.shape [0], v[0, 1]-v[0, 0]), v.shape[0]*(v[0, 1]-v[0, 0]), i, arr[v[0, 0]:v[0, 1]]) for i, v in enumerate(l, 0)]
-        l_sorted = sorted(l_info, key=lambda x: x[1], reverse=True)
-        
-        l_chosen_index = [l_sorted[0][2]]
-        l_chosen_unique_bytes = [l_sorted[0][3]]
-        
-        LEN_CHOSEN_INDEX = 16
-        for _, _, i, unique_bytes in l_sorted:
-            print("unique_bytes: {}".format(unique_bytes))
-            
-            is_crossover_possible = False
-            for unique_bytes_2 in l_chosen_unique_bytes:
-                if utils_compress_enwik8.check_if_crossover_is_possible(unique_bytes, unique_bytes_2):
-                    is_crossover_possible = True
-                    break
-            if is_crossover_possible:
-                print("Ignore: i: {}, unique_bytes: {}".format(i, unique_bytes))
+            for unique_bytes, amount in zip(u, c):
+                t = tuple(unique_bytes.tolist())
+                d_word_occurences[t] = amount
+                # d_word_weight[t] = amount*2**len(t)
+                # d_word_weight[t] = amount*2**(len(bin(len(t))[2:])-1)
+                # d_word_weight[t] = amount*2**len(bin(len(t))[2:])
+                # d_word_weight[t] = amount*len(t)
+                d_word_weight[t] = amount*len(bin(len(t))[2:])
+
+        # l_occurences_sorted = sorted([(v, k) for k, v in d_word_occurences.items()], reverse=True)
+        # l_weight_sorted = sorted([(v, k) for k, v in d_word_weight.items()], reverse=True)
+
+
+        unique_bytes_index = 0
+        # l_chosen_unique_bytes_prev = []
+        l_chosen_unique_bytes_index = []
+        l_chosen_unique_bytes_diff = []
+        l_chosen_unique_bytes_ranges = []
+        d_chosen_unique_bytes_to_index = {}
+        l_used_idxs_bool = []
+        last_index = 0
+        length = arr.shape[0]
+        pos = 0
+        while pos < length:
+            # print("pos: {}".format(pos))
+
+            # find the best unique byte for the next occurence!
+            l_tpls = []
+            l_weights = []
+            for j in range(2, MAX_BYTE_LENGTH):
+                t = tuple(arr[pos:pos+j].tolist())
+                if t in d_word_weight:
+                    l_weights.append(d_word_weight[t])
+                    l_tpls.append(t)
+
+            if len(l_weights)==0:
+                pos += 1
                 continue
+            
+            # best_word_length = np.argmax(l_weights)+2
+            # t = tuple(arr[pos:pos+best_word_length].tolist())
+            t = l_tpls[np.argmax(l_weights)]
 
-            l_chosen_index.append(i)
-            l_chosen_unique_bytes.append(unique_bytes)
+            if not t in d_chosen_unique_bytes_to_index:
+                if unique_bytes_index<LEN_CHOSEN_INDEX:
+                    d_chosen_unique_bytes_to_index[t] = unique_bytes_index
+                    # l_chosen_unique_bytes_prev.append(t)
+                    unique_bytes_index += 1
+                else:
+                    pos += 1
+                    continue
 
-            if len(l_chosen_index)==LEN_CHOSEN_INDEX:
-            # if len(l_chosen_index)==8:
-                break
+            len_t = len(t)
+            l_chosen_unique_bytes_index.append(d_chosen_unique_bytes_to_index[t])
+            l_chosen_unique_bytes_diff.append(pos-last_index)
+            l_chosen_unique_bytes_ranges.append((pos, pos+len_t))
+            last_index = pos+len_t
+            pos = last_index
 
-        assert len(l_chosen_index)==LEN_CHOSEN_INDEX
-        # l_chosen_index = [i for _, _, i, _ in l_sorted[:8]]
-        l_chosen_idxs_ranges = [l[i] for i in l_chosen_index]
-        idxs_ranges_combined = np.vstack(l_chosen_idxs_ranges) 
-        idxs_ranges_sorted = idxs_ranges_combined[np.argsort(idxs_ranges_combined[:, 0])]
-        assert np.all(idxs_ranges_sorted[1:, 0]>=idxs_ranges_sorted[:-1, 1])
-        
-        arr_diff = np.hstack(((idxs_ranges_sorted[0, 0], ), idxs_ranges_sorted[1:, 0]-idxs_ranges_sorted[:-1, 1]))
-        arr_diff_bits_amount = [len(bin(v)[2:]) if v>0 else 0 for v in arr_diff]
+        assert unique_bytes_index==LEN_CHOSEN_INDEX
 
-        # first calculate, how many bytes there really are!
-        l_amounts = [l_info[i][1] for i in l_chosen_index]
-        bytes_amount_raw = sum(l_amounts)
-        bits_amount_raw = bytes_amount_raw*8
+        # assert check
+        # d_chosen_unique_bytes_from_index = {v: k for k, v in d_chosen_unique_bytes_to_index.items()}
+        l_chosen_unique_bytes = [v for _, v in sorted([(v, k) for k, v in d_chosen_unique_bytes_to_index.items()])]
+        # assert l_chosen_unique_bytes==l_chosen_unique_bytes_prev
+        pos = 0
+        for i in range(0, len(l_chosen_unique_bytes_index)):
+            ub_index = l_chosen_unique_bytes_index[i]
+            ub_tpl = l_chosen_unique_bytes[ub_index]
+            # ub_tpl = d_chosen_unique_bytes_from_index[ub_index]
 
-        # generate a mapping index of the unique bytes
-        l_chosen_ub_tbl = [tuple(v.tolist()) for v in l_chosen_unique_bytes]
+            ub_diff = l_chosen_unique_bytes_diff[i]
+            pos += ub_diff
+            length = len(ub_tpl)
 
-        d_chosen_ub_tbl = {t: i for i, t in enumerate(l_chosen_ub_tbl, 0)}
-        l_idx_tbl = [d_chosen_ub_tbl[tuple(arr[i1:i2].tolist())] for i1, i2 in idxs_ranges_sorted]
+            ub_arr = arr[pos:pos+length]
+            assert tuple(ub_arr.tolist())==ub_tpl
+            pos += len(ub_tpl)
 
-        length = len(l_idx_tbl)
+        l_diff_bits_amount = [len(bin(v)[2:]) if v>0 else 0 for v in l_chosen_unique_bytes_diff]
+
+        length = len(l_chosen_unique_bytes_index)
         length_needed_bytes = (lambda x: (x+x%2)//2)(len(hex(length)[2:]))
 
-        content_bits_compressed = (
-            bin(len(l_chosen_ub_tbl)-1)[2:].zfill(4)+
+        content_bits_compressed_str = (
+            bin(MAX_BYTE_LENGTH)[2:].zfill(8)+
             ''.join([bin(len(v)-1)[2:].zfill(4) for v in l_chosen_unique_bytes])+
             ''.join([bin(v)[2:].zfill(8) for v in np.hstack(l_chosen_unique_bytes)])+
             bin(length_needed_bytes-1)[2:].zfill(2)+
             bin(length)[2:].zfill(8*length_needed_bytes)+
-            ''.join([bin(i)[2:].zfill(4) for i in l_idx_tbl])+
-            ''.join([bin(i)[2:].zfill(4) for i in arr_diff_bits_amount])+
-            ''.join([bin(v)[2:] for v in arr_diff if v>0])
+            ''.join([bin(i)[2:].zfill(LEN_BITS_CHOSEN_INDEX) for i in l_chosen_unique_bytes_index])+
+            ''.join([bin(i)[2:].zfill(4) for i in l_diff_bits_amount])+
+            ''.join([bin(v)[2:] for v in l_chosen_unique_bytes_diff if v>0])
         )
-        print("len(content_bits_compressed): {}".format(len(content_bits_compressed)))
-        print("bits_amount_raw: {}".format(bits_amount_raw))
 
-        if len(content_bits_compressed)%8!=0:
-            length_content = len(content_bits_compressed)
-            content_bits_compressed += '0'*(8-(length_content%8))
+        len_bytes_raw = sum([len(l_chosen_unique_bytes[i]) for i in l_chosen_unique_bytes_index])
+        len_bits_raw = len_bytes_raw*8
 
-        arr_content_compressed = np.sum(np.array(list(map(int, list(content_bits_compressed)))).reshape((-1, 8))*2**np.arange(7, -1, -1), axis=1)
+        print("l_chosen_unique_bytes: {}".format(l_chosen_unique_bytes))
+        l_lens = list(map(len, l_chosen_unique_bytes))
+        print("l_lens: {}".format(l_lens))
+
+
+        if len(content_bits_compressed_str)%8!=0:
+            length_content = len(content_bits_compressed_str)
+            content_bits_compressed_str += '0'*(8-(length_content%8))
+        len_bits_compressed = len(content_bits_compressed_str)
+        
+        print("len_bits_compressed: {}".format(len_bits_compressed))
+        print("len_bits_raw: {}".format(len_bits_raw))
+
+        l_len_raw_bits.append(len_bits_raw)
+        l_len_compressed_bits.append(len_bits_compressed)
+
+        arr_content_compressed = np.sum(np.array(list(map(int, list(content_bits_compressed_str)))).reshape((-1, 8))*2**np.arange(7, -1, -1), axis=1)
         arr_content_compressed_uint8 = arr_content_compressed.astype(np.uint8)
 
         arr_content_compressed_uint8.tofile('content_compressed_round_nr_{}.hex'.format(round_nr))
 
-        idxs_rest = ~np.isin(np.arange(0, len(arr)), np.hstack([np.arange(i1, i2) for i1, i2 in idxs_ranges_sorted]))
+        idxs_rest = ~np.isin(np.arange(0, len(arr)), np.hstack([np.arange(i1, i2) for i1, i2 in l_chosen_unique_bytes_ranges]))
+        # idxs_rest = ~np.isin(np.arange(0, len(arr)), np.hstack([np.arange(i1, i2) for i1, i2 in idxs_ranges_sorted]))
         arr = arr[idxs_rest]
 
-        l_content_bits_compressed.append(content_bits_compressed)
+        l_content_bits_compressed.append(content_bits_compressed_str)
         l_arr_content_compressed_uint8.append(arr_content_compressed_uint8)
 
-        print("Rest: len(arr): {}".format(len(arr)))
+    print()
+    print("bytes_starting_size: {}".format(bytes_starting_size))
+    print("LEN_BITS_CHOSEN_INDEX: {}".format(LEN_BITS_CHOSEN_INDEX))
+    print("LEN_CHOSEN_INDEX: {}".format(LEN_CHOSEN_INDEX))
+    print("MAX_BYTE_LENGTH: {}".format(MAX_BYTE_LENGTH))
+    print("ROUNDS_AMOUNT: {}".format(ROUNDS_AMOUNT))
+    
+    print("l_len_raw_bits: {}".format(l_len_raw_bits))
+    print("l_len_compressed_bits: {}".format(l_len_compressed_bits))
+
+    sum_raw_bits = sum(l_len_raw_bits)
+    sum_compressed_bits = sum(l_len_compressed_bits)
+    percent_compression = ((sum_raw_bits-sum_compressed_bits)/sum_raw_bits)*100
+
+    print("sum_raw_bits: {}".format(sum_raw_bits))
+    print("sum_compressed_bits: {}".format(sum_compressed_bits))
+    print("percent_compression: {:.06f} %".format(percent_compression))
+    sys.exit()
+
+
+
+    l = []
+    for byte_amount in range(2, MAX_BYTE_LENGTH+1):
+        print("byte_amount: {}".format(byte_amount))
+        arr_comb = d_arr_comb[byte_amount]
+        for i_unique, unique_bytes in enumerate(d_arr_comb_unique[byte_amount]['u'][:20], 0):
+            print("i_unique: {}".format(i_unique))
+            idxs_nr = np.where(np.all(arr_comb==unique_bytes, axis=1))[0]
+            idxs_ranges = np.vstack((idxs_nr, idxs_nr+byte_amount)).T
+            l_idxs_ranges = utils_compress_enwik8.get_list_of_ranges(idxs_ranges)
+            l.append(l_idxs_ranges[0])
+
+    l_info = [((v.shape [0], v[0, 1]-v[0, 0]), v.shape[0]*(v[0, 1]-v[0, 0]), i, arr[v[0, 0]:v[0, 1]]) for i, v in enumerate(l, 0)]
+    l_sorted = sorted(l_info, key=lambda x: x[1], reverse=True)
+    
+    l_chosen_index = [l_sorted[0][2]]
+    l_chosen_unique_bytes = [l_sorted[0][3]]
+    
+    LEN_CHOSEN_INDEX = 16
+    for _, _, i, unique_bytes in l_sorted:
+        print("unique_bytes: {}".format(unique_bytes))
+        
+        is_crossover_possible = False
+        for unique_bytes_2 in l_chosen_unique_bytes:
+            if utils_compress_enwik8.check_if_crossover_is_possible(unique_bytes, unique_bytes_2):
+                is_crossover_possible = True
+                break
+        if is_crossover_possible:
+            print("Ignore: i: {}, unique_bytes: {}".format(i, unique_bytes))
+            continue
+
+        l_chosen_index.append(i)
+        l_chosen_unique_bytes.append(unique_bytes)
+
+        if len(l_chosen_index)==LEN_CHOSEN_INDEX:
+        # if len(l_chosen_index)==8:
+            break
+
+    assert len(l_chosen_index)==LEN_CHOSEN_INDEX
+    # l_chosen_index = [i for _, _, i, _ in l_sorted[:8]]
+    l_chosen_idxs_ranges = [l[i] for i in l_chosen_index]
+    idxs_ranges_combined = np.vstack(l_chosen_idxs_ranges) 
+    idxs_ranges_sorted = idxs_ranges_combined[np.argsort(idxs_ranges_combined[:, 0])]
+    assert np.all(idxs_ranges_sorted[1:, 0]>=idxs_ranges_sorted[:-1, 1])
+    
+    arr_diff = np.hstack(((idxs_ranges_sorted[0, 0], ), idxs_ranges_sorted[1:, 0]-idxs_ranges_sorted[:-1, 1]))
+    arr_diff_bits_amount = [len(bin(v)[2:]) if v>0 else 0 for v in arr_diff]
+
+    # first calculate, how many bytes there really are!
+    l_amounts = [l_info[i][1] for i in l_chosen_index]
+    bytes_amount_raw = sum(l_amounts)
+    bits_amount_raw = bytes_amount_raw*8
+
+    # generate a mapping index of the unique bytes
+    l_chosen_ub_tbl = [tuple(v.tolist()) for v in l_chosen_unique_bytes]
+
+    d_chosen_ub_tbl = {t: i for i, t in enumerate(l_chosen_ub_tbl, 0)}
+    l_idx_tbl = [d_chosen_ub_tbl[tuple(arr[i1:i2].tolist())] for i1, i2 in idxs_ranges_sorted]
+
+    length = len(l_idx_tbl)
+    length_needed_bytes = (lambda x: (x+x%2)//2)(len(hex(length)[2:]))
+
+    content_bits_compressed = (
+        bin(len(l_chosen_ub_tbl)-1)[2:].zfill(4)+
+        ''.join([bin(len(v)-1)[2:].zfill(4) for v in l_chosen_unique_bytes])+
+        ''.join([bin(v)[2:].zfill(8) for v in np.hstack(l_chosen_unique_bytes)])+
+        bin(length_needed_bytes-1)[2:].zfill(2)+
+        bin(length)[2:].zfill(8*length_needed_bytes)+
+        ''.join([bin(i)[2:].zfill(4) for i in l_idx_tbl])+
+        ''.join([bin(i)[2:].zfill(4) for i in arr_diff_bits_amount])+
+        ''.join([bin(v)[2:] for v in arr_diff if v>0])
+    )
+    print("len(content_bits_compressed): {}".format(len(content_bits_compressed)))
+    print("bits_amount_raw: {}".format(bits_amount_raw))
+
+    if len(content_bits_compressed)%8!=0:
+        length_content = len(content_bits_compressed)
+        content_bits_compressed += '0'*(8-(length_content%8))
+
+    arr_content_compressed = np.sum(np.array(list(map(int, list(content_bits_compressed)))).reshape((-1, 8))*2**np.arange(7, -1, -1), axis=1)
+    arr_content_compressed_uint8 = arr_content_compressed.astype(np.uint8)
+
+    arr_content_compressed_uint8.tofile('content_compressed_round_nr_{}.hex'.format(round_nr))
+
+    idxs_rest = ~np.isin(np.arange(0, len(arr)), np.hstack([np.arange(i1, i2) for i1, i2 in idxs_ranges_sorted]))
+    arr = arr[idxs_rest]
+
+    l_content_bits_compressed.append(content_bits_compressed)
+    l_arr_content_compressed_uint8.append(arr_content_compressed_uint8)
+
+    print("Rest: len(arr): {}".format(len(arr)))
 
     np.hstack(l_arr_content_compressed_uint8).tofile('content_compressed_full.hex')
 
