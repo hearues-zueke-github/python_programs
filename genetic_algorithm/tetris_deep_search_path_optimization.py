f63fde1c0ec6ee314f3b354121419e9cfd21b6bd
Hearues Zueke <hziko314@gmail.com>
2020-05-24 19:58:58 +0200

    add more stuff for better finding the position of the current and given next piece

--- genetic_algorithm/tetris_deep_search_path_optimization.py
+++ genetic_algorithm/tetris_deep_search_path_optimization.py
@@ -4,6 +4,7 @@
 
 import dill
 import functools
+import itertools
 import os
 import random
 import sys
@@ -23,6 +24,8 @@ import tkinter.ttk as ttk
 import multiprocessing as mp
 from multiprocessing import Process, Pipe # , Lock
 from recordclass import recordclass, RecordClass
+
+import matplotlib.pyplot as plt
 
 from threading import Lock
 
@@ -128,6 +131,7 @@ class TetrisGeneticAlgorithm(Exception):
         self.d_piece_name_counter = {piece_name: 0 for piece_name in self.l_piece_name}
         self.clear_lines_total = 0
         self.clear_lines = 0
+        self.max_height = 0
 
         # the amount of boxes in fields + 7 nodes for currecnt piece + 7 nodes for next piece
         self.input_nodes = self.field_columns*self.field_rows+7+7
@@ -193,6 +197,7 @@ class TetrisGeneticAlgorithm(Exception):
         
         self.clear_lines_total = 0
         self.clear_lines = 0
+        self.max_height = 0
 
 
     def do_move_piece_down_instant(self):
@@ -254,18 +259,20 @@ class TetrisGeneticAlgorithm(Exception):
     def do_next_piece(self, piece_name, output_node):
         self.define_next_piece(piece_name, output_node)
         is_piece_moved = self.do_move_piece_down_instant()
+        self.clear_lines = 0
 
         # print("self.field:\n{}".format(self.field))
 
         if not is_piece_moved and self.need_field_reset:
-            self.reset_field()
+            return
+            # self.reset_field()
 
         idxs_row_full = np.all(self.field!=0, axis=1)
         if np.any(idxs_row_full):
             self.field[idxs_row_full] = 0
 
-            self.clear_lines += np.sum(idxs_row_full)
-            self.clear_lines_total = np.sum(idxs_row_full)
+            self.clear_lines_total += np.sum(idxs_row_full)
+            self.clear_lines = np.sum(idxs_row_full)
 
             all_rows_full = np.where(idxs_row_full)[0]
             all_rows_empty = np.where(np.all(self.field==0, axis=1))[0]
@@ -286,10 +293,18 @@ class TetrisGeneticAlgorithm(Exception):
                     self.field[row_to] = self.field[row_from]
                     self.field[row_from] = 0
 
+        idxs_pos = np.where(np.flip(np.any(self.field!=0, axis=1)))[0]
+        if idxs_pos.shape[0]==0:
+            self.max_height = 0
+        else:
+            self.max_height = idxs_pos[-1]+1
+
 
 if __name__ == "__main__":
-    field_columns = 10
-    field_rows = 20
+    field_columns = 5
+    field_rows = 15
+    # field_columns = 10
+    # field_rows = 25
     field_add_rows = 4
     field_rows_total = field_rows+field_add_rows
     tetris = TetrisGeneticAlgorithm(field_columns, field_rows, field_add_rows)
@@ -309,61 +324,126 @@ if __name__ == "__main__":
     field_weights += np.flip(np.cumsum(np.arange(1, field_rows_total+1)).reshape((-1, 1)))
     print("field_weights:\n{}".format(field_weights))
 
-    d_t_idxs_to_weights_sum = {}
-    
-    l_piece_name_order = ['I', 'J', 'T']
-    def calc_best_output_nodes(l_piece_name_order):
-        d = {}
-        d['best_min_weight'] = np.sum(field_weights)
-        d['best_t_idxs'] = ()
+    l_best_sequence_piece_name_idx = []
+    amount_pieces = 100
+    l_sequence_piece_name = [tetris.l_piece_name[i] for i in np.random.randint(0, 7, (amount_pieces+1, ))]
+    print("l_sequence_piece_name: {}".format(l_sequence_piece_name))
+    # sys.exit()
 
-        max_i = len(l_piece_name_order)-1
+    # l_piece_name_order_first = ['I', 'J']
+    amount_next_pieces = 1
+    for i in range(0, len(l_sequence_piece_name)-amount_next_pieces-1):
+        l_piece_name_order_first = l_sequence_piece_name[i:i+amount_next_pieces+1]
+        
+        d_t_idxs_to_weights_sum_clear_lines_max_height = {}
+        for piece_name_last in tetris.l_piece_name:
+            def calc_weights(l_piece_name_order):
+                max_i = len(l_piece_name_order)-1
 
-        def calc_best_output_nodes_intern(i, t_idxs):
-            piece_name = l_piece_name_order[i]
-            amount_output_nodes = d_piece_name_to_amount_output_nodes[piece_name]
-            for output_node in range(0, amount_output_nodes):
-                prev_field = tetris.field.copy()
-                tetris.do_next_piece(piece_name=piece_name, output_node=output_node)
-                
-                t_idxs_now = t_idxs+(output_node, )
-                
-                if i==max_i:
-                    weights_sum = np.sum(field_weights[tetris.field!=0])
-                    
-                    if d['best_min_weight']>weights_sum:
-                        d['best_min_weight'] = weights_sum
-                        d['best_t_idxs'] = t_idxs_now
+                def calc_weights_intern(i, t_idxs, clear_lines):
+                    piece_name = l_piece_name_order[i]
+                    amount_output_nodes = d_piece_name_to_amount_output_nodes[piece_name]
+                    for output_node in range(0, amount_output_nodes):
+                        prev_field = tetris.field.copy()
+                        tetris.do_next_piece(piece_name=piece_name, output_node=output_node)
+                        
+                        clear_lines_new = clear_lines+tetris.clear_lines
 
-                    d_t_idxs_to_weights_sum[t_idxs_now] = weights_sum
-                else:
-                    calc_best_output_nodes_intern(i+1, t_idxs_now)
+                        t_idxs_now = t_idxs+(output_node, )
+                        
+                        if i==max_i:
+                            weights_sum = np.sum(field_weights[tetris.field!=0])
+                            max_height = tetris.max_height
+                            d_t_idxs_to_weights_sum_clear_lines_max_height[t_idxs_now] = (weights_sum, clear_lines_new, max_height)
+                        else:
+                            calc_weights_intern(i+1, t_idxs_now, clear_lines_new)
 
-                tetris.field[:] = prev_field
-        calc_best_output_nodes_intern(0, ())
+                        tetris.field[:] = prev_field
+                calc_weights_intern(0, (piece_name_last, ), clear_lines=0)
 
-        return d['best_min_weight'], d['best_t_idxs']
+            l_piece_name_order = l_piece_name_order_first+[piece_name_last]
+            calc_weights(l_piece_name_order)
+        
 
-    best_min_weight, best_t_idxs = calc_best_output_nodes(l_piece_name_order)
-    
-    # print("d_t_idxs_to_weights_sum: {}".format(d_t_idxs_to_weights_sum))
-    print("best_min_weight: {}".format(best_min_weight))
-    print("best_t_idxs: {}".format(best_t_idxs))
-    print("l_piece_name_order: {}".format(l_piece_name_order))
+        # pn1 = l_piece_name_order_first[0]
+        # pn2 = l_piece_name_order_first[1]
+        l_t_idxs_combined = list(itertools.product(*[range(0, d_piece_name_to_amount_output_nodes[pn]) for pn in l_piece_name_order_first]))
+        
+        l_idxs = []
+        l_weights_average = []
 
-    # first, combine the average of all weights for the rest found parts
-    # we assume, we only see two pieces: the current one, and the next following!
-    # our third one is the prediction, but the third one can change too!
+        # for idx1 in range(0, d_piece_name_to_amount_output_nodes[pn1]):
+        #     for idx2 in range(0, d_piece_name_to_amount_output_nodes[pn2]):
+        for t_idxs_combined in l_t_idxs_combined:
+                max_height_sum = 0
+                clear_lines_sum = 0
+                weight_sum = 0
+                amount_values = 0
 
-    pn1 = l_piece_name_order[0]
-    pn2 = l_piece_name_order[1]
-    pn3 = l_piece_name_order[2]
-    
+                for pn_last in tetris.l_piece_name:
+                    max_clear_lines = 0
+                    min_weight = np.sum(field_weights)
+                    min_max_height = field_rows
+                    for idx_last in range(0, d_piece_name_to_amount_output_nodes[pn_last]):
+                        t_combined = (pn_last, )+t_idxs_combined+(idx_last, )
+                        weight, clear_lines, max_height = d_t_idxs_to_weights_sum_clear_lines_max_height[t_combined]
+                        # weight, clear_lines, max_height = d_t_idxs_to_weights_sum_clear_lines_max_height[(pn_last, idx1, idx2, idx_last)]
+
+                        # if min_max_height > max_height:
+                        #     min_max_height = max_height
+                        #     max_clear_lines = clear_lines
+                        #     min_weight = weight
+                        if max_clear_lines < clear_lines:
+                            max_clear_lines = clear_lines
+                            min_weight = weight
+                        elif min_weight > weight:
+                            min_weight = weight
+
+                    max_height_sum += min_max_height
+                    clear_lines_sum += max_clear_lines
+                    weight_sum += min_weight
+                    amount_values += 1
+
+                l_idxs.append(t_idxs_combined)
+                # l_idxs.append([idx1, idx2])
+                l_weights_average.append(weight_sum/amount_values)
+
+        best_t_idxs = l_idxs[np.argmin(l_weights_average)]
+
+        t = (l_piece_name_order_first[0], best_t_idxs[0])
+        tetris.do_next_piece(piece_name=t[0], output_node=t[1])
+        print("tetris.field:\n{}".format(tetris.field))
+        print("i: {}, t: {}, max_height: {}".format(i, t, tetris.max_height))
+
+        l_best_sequence_piece_name_idx.append(t)
+
 
     prev_field = tetris.field.copy()
-    for step_i, (piece_name, output_node) in enumerate(zip(l_piece_name_order, best_t_idxs), 1):
+    tetris.reset_field()
+    print("\n")
+    l_max_height = []
+    for step_i, (piece_name, output_node) in enumerate(l_best_sequence_piece_name_idx, 1):
+        tetris.do_next_piece(piece_name=piece_name, output_node=output_node)
+        idxs_pos = np.where(np.flip(np.any(tetris.field!=0, axis=1)))[0]
+        if idxs_pos.shape[0]==0:
+            max_height = 0
+        else:
+            max_height = idxs_pos[-1]+1
+        l_max_height.append(max_height)
         print("piece_name: {}, output_node: {}".format(piece_name, output_node))
         print("step_i: {}, tetris.field:\n{}".format(step_i, tetris.field))
-        tetris.do_next_piece(piece_name=piece_name, output_node=output_node)
-    # print("tetris.field:\n{}".format(tetris.field))
     tetris.field[:] = prev_field
+
+    print("l_max_height: {}".format(l_max_height))
+
+    # print a simple graph, where the max height of each step is shown!
+    plt.figure()
+
+    plt.plot(np.arange(0, len(l_max_height)), l_max_height, 'b.-')
+
+    plt.show()
+
+    # TODO: next, add a conversion from given field, piece_currenct and piece_next
+    # into input_vector and output_vector for the neural network learning datas!
+    # TODO: save the given input_vector and output_vector for the piece_current and piece_next
+    # into files for the givene (rows, cols) of the field!
