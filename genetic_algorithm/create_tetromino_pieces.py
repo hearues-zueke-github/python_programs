5ed58637116eebe7bdcb369f4bd45d63d5f7517e
Hearues Zueke <hziko314@gmail.com>
2020-05-27 01:30:33 +0200

    add more stuff, plus can now calculate dynamically tetronimos pieces

---
+++ genetic_algorithm/create_tetromino_pieces.py
@@ -0,0 +1,133 @@
+#! /usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+import dill
+import functools
+import itertools
+import os
+import random
+import sys
+import time
+
+from copy import deepcopy
+
+import numpy as np
+
+from PIL import Image, ImageTk
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+import multiprocessing as mp
+from multiprocessing import Process, Pipe # , Lock
+from recordclass import recordclass, RecordClass
+
+import matplotlib.pyplot as plt
+
+from threading import Lock
+
+import base64
+import json
+
+from memory_tempfile import MemoryTempfile
+tempfile = MemoryTempfile()
+
+import platform
+print("platform.system(): {}".format(platform.system()))
+
+PATH_ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")+"/"
+
+if __name__ == "__main__":
+    l_piece_pos = [((0, 0), )] # base case
+
+    def get_all_rotation_piece_pos(piece_pos):
+        l_piece_pos_rotations = [piece_pos]
+        for _ in range(0, 3):
+            arr = np.array(piece_pos)
+            arr = np.flip(arr, axis=1)
+            arr[:, 1] = np.max(arr[:, 1])-arr[:, 1]
+            piece_pos_rot = tuple(sorted(tuple(t for t in l) for l in arr.tolist()))
+            piece_pos = piece_pos_rot
+            l_piece_pos_rotations.append(piece_pos)
+
+        return tuple(sorted(set(l_piece_pos_rotations)))
+    
+    piece_pos = ((0, 0), (0, 1), (1, 1), (1, 2)) # S piece
+    t_piece_pos = get_all_rotation_piece_pos(piece_pos)
+    
+    print("piece_pos: {}".format(piece_pos))
+    print("t_piece_pos: {}".format(t_piece_pos))
+
+    d_l_piece_pos = {1: l_piece_pos}
+    d_l_piece_pos_rotate_group = {}
+
+    l_amount_rotated_pieces = []
+    l_amount_unique_pieces = []
+
+    for i in range(2, 7):
+        l_pieces_pos_new = []
+        for piece_pos in l_piece_pos:
+            s_new_pos = set()
+            for y, x in piece_pos:
+                t = (y-1, x)
+                if t not in s_new_pos and t not in piece_pos:
+                    s_new_pos.add(t)
+                t = (y+1, x)
+                if t not in s_new_pos and t not in piece_pos:
+                    s_new_pos.add(t)
+                t = (y, x-1)
+                if t not in s_new_pos and t not in piece_pos:
+                    s_new_pos.add(t)
+                t = (y, x+1)
+                if t not in s_new_pos and t not in piece_pos:
+                    s_new_pos.add(t)
+
+            l_combined_pieces = []
+            for new_pos in s_new_pos:
+                l_combined_pieces.append(piece_pos+(new_pos, ))
+
+            arr = np.array(l_combined_pieces)
+            arr -= np.min(arr, axis=1).reshape((-1, 1, 2))
+
+            l_combined_pieces_moved = [tuple(sorted(tuple(t for t in l) for l in l2)) for l2 in arr.tolist()]
+            s_combined_pieces = set(l_combined_pieces_moved)
+
+            l_pieces_pos_new.extend(s_combined_pieces)
+
+        l_pieces_pos_new_unique = sorted(set(l_pieces_pos_new))
+        l_piece_pos = l_pieces_pos_new_unique
+        print("i: {}".format(i))
+        print("len(l_piece_pos): {}".format(len(l_piece_pos)))
+
+        # create different shape groups! need to rotate each shape btw.
+        l_piece_pos_rotate_group = sorted(list(set(get_all_rotation_piece_pos(piece_pos) for piece_pos in l_piece_pos)), key=lambda x: (len(x), x))
+        print("len(l_piece_pos_rotate_group): {}".format(len(l_piece_pos_rotate_group)))
+
+        l_amount_rotated_pieces.append(len(l_piece_pos))
+        l_amount_unique_pieces.append(len(l_piece_pos_rotate_group))
+
+        d_l_piece_pos[i] = l_piece_pos
+        d_l_piece_pos_rotate_group[i] = l_piece_pos_rotate_group
+
+    print("l_amount_rotated_pieces: {}".format(l_amount_rotated_pieces))
+    print("l_amount_unique_pieces: {}".format(l_amount_unique_pieces))
+
+
+    def save_l_piece_pos_rotate_group_to_file(l_piece_pos_rotate_group):
+        num_blocks = len(l_piece_pos_rotate_group[0][0])
+        num_groups = len(l_piece_pos_rotate_group)
+        l_group_len = [len(l) for l in l_piece_pos_rotate_group]
+        l_pos = [v for t1 in l_piece_pos_rotate_group for t2 in t1 for t3 in t2 for v in t3]
+        l = [num_blocks, num_groups]+l_group_len+l_pos
+        return l
+
+    # for num_blocks in range(2, 7):
+    num_blocks = 4
+    l_piece_pos_rotate_group = d_l_piece_pos_rotate_group[num_blocks]
+    l = save_l_piece_pos_rotate_group_to_file(l_piece_pos_rotate_group)
+    print("num_blocks: {}, len(l): {}".format(num_blocks, len(l)))
+
+    bytes_content = bytes(l)
+    with open(PATH_ROOT_DIR+'tetris_pieces_block_amount_4.trpcs', 'wb') as f:
+        f.write(bytes_content)
--- genetic_algorithm/tetris_deep_search_path_optimization.cpp
+++ genetic_algorithm/tetris_deep_search_path_optimization.cpp
@@ -8,6 +8,7 @@
 #include <tuple>
 #include <iterator>
 #include <random>
+#include <exception>
 
 #include "../cpp_programs/primes/utils.h"
 
@@ -38,7 +39,7 @@ Iter select_randomly(Iter start, Iter end) {
     return select_randomly(start, end, gen);
 }
 
-class TetrisField {
+class TetrisField : public std::exception {
 private:
   uint8_t* field_arr_;
   uint8_t** field_;
@@ -46,23 +47,63 @@ private:
   int rows_;
   int columns_;
 
-  struct PiecePositions {
-    string name_;
-    string direction_;
-    int min_x_;
-    int max_x_;
-    int pos_[4][2];
+  typedef std::tuple<string, string, int> key_name_dir_pos;
 
-    void printAll() {
-      ostringstream ss;
-      ss << "name_: " << name_ << endl;
-      string s = ss.str();
-      cout << s;
+  struct Position {
+    int rows_;
+    int columns_;
+    // int* pos_arr_;
+    // int** pos_;
+    vector<vector<int>> pos_;
+
+    Position() {
+    }
+
+    void setPositonParams(const int rows, const int columns) {
+    // Position (const int rows, const int columns) {
+      rows_ = rows;
+      columns_ = columns;
+      // pos_arr_ = new int[rows * columns];
+      // pos_ = new int*[rows];
+      // for (int i = 0; i < rows; ++i) {
+      //   pos_[i] = pos_arr_ + columns * i;
+      // }
+    }
+
+    ~Position() {
+      // if (pos_arr_ != nullptr) {
+      //   delete[] pos_arr_;
+      //   pos_arr_ = nullptr;
+      // }
+      // if (pos_arr_ != nullptr) {
+      //   delete[] pos_;
+      //   pos_ = nullptr;
+      // }
+    }
+
+    // TODO: fix this const later!
+    void copyPos(const vector<vector<int>>& pos) {
+      pos_ = pos;
+
+      // for (int j = 0; j < rows_; ++j) {
+      //   for (int i = 0; i < columns_; ++i) {
+      //     pos_arr_[j * columns_ + i] = pos_arr[j][i];
+      //     // cout << "pos_arr_[i]: " << pos_arr_[i] << ", pos_arr[i]: " << pos_arr[i] << endl;
+      //   }
+      // }
+      // std::copy(&pos_arr, &pos_arr + rows_ * columns_, &pos_arr_);
+    }
+
+    void copyPos(const Position& obj) {
+      pos_ = obj.pos_;
+      // std::copy(&obj.pos_arr_, &obj.pos_arr_ + rows_ * columns_, &pos_arr_);
     }
 
     void printPos() {
       ostringstream ss;
-      for (int i = 0; i < 4; ++i) {
+      cout << "pos_: " << pos_ << endl;
+      for (int i = 0; i < rows_; ++i) {
+        // ss << "(" << pos_arr_[i*2+0] << ", " << pos_arr_[i*2+1] << "), ";
         ss << "(" << pos_[i][0] << ", " << pos_[i][1] << "), ";
       }
       string s = ss.str();
@@ -70,26 +111,59 @@ private:
       cout << "pos_: " << s << endl;
     }
   };
-  PiecePositions piece_positions_[19];
 
-  struct AbsStartPiecePositions {
-    int pos_[4][2];
+  struct Piece : public Position {
+    key_name_dir_pos key_;
+    bool is_piece_set_;
+    string name_;
+    string direction_;
+    int pos_place_;
+    int idx_;
 
-    void copyPos(int pos[4][2]) {
-      for (int i = 0; i < 4; ++i) {
-        pos_[i][0] = pos[i][0];
-        pos_[i][1] = pos[i][1];
-      }
+    Piece() {
+    }
+  };
+  Piece current_piece_;
+
+  struct PiecePositions : public Position {
+    string name_;
+    string direction_;
+    int min_x_;
+    int max_x_;
+    int idx_;
+
+    PiecePositions() {
     }
 
-    void printPos() {
+    PiecePositions(string name, string direction, int min_x, int max_x) :
+        name_(name), direction_(direction), min_x_(min_x), max_x_(max_x) {
+    }
+
+    void setPiecePosition(const vector<vector<int>>& pos) {
+      const int rows = pos.size();
+      const int columns= pos[0].size();
+      setPositonParams(rows, columns);
+      copyPos(pos);
+    }
+
+    ~PiecePositions() {
+    }
+
+    void printProperties() {
       ostringstream ss;
-      for (int i = 0; i < 4; ++i) {
-        ss << "(" << pos_[i][0] << ", " << pos_[i][1] << "), ";
-      }
+      ss << "1 name_: " << name_ << endl;
+      ss << "2 direction_: " << direction_ << endl;
+      ss << "3 min_x_: " << min_x_ << endl;
+      ss << "4 max_x_: " << max_x_ << endl;
+      ss << "5 idx_: " << idx_ << endl;
       string s = ss.str();
-      s.resize(s.size() - 2);
-      cout << "pos_: " << s << endl;
+      cout << s;
+    }
+  };
+  PiecePositions piece_positions_[19];
+
+  struct AbsStartPiecePositions : public Position {
+    AbsStartPiecePositions() {
     }
   };
   
@@ -98,11 +172,12 @@ private:
   map<string, uint8_t> piece_name_to_idx_;
   map<string, uint8_t> piece_name_to_index_;
 
-  typedef std::tuple<string, string, int> key_name_dir_pos;
   map<key_name_dir_pos, AbsStartPiecePositions> piece_name_direction_pos_to_abs_pos_;
   vector<key_name_dir_pos> keys_;
 public:
   TetrisField(const int rows, const int columns) {
+    current_piece_.is_piece_set_ = false;
+
     rows_ = rows;
     columns_ = columns;
 
@@ -118,37 +193,67 @@ public:
       piece_name_to_idx_[pieces_name_[i]] = pieces_idx_[i];
     }
 
-    piece_positions_[0] = {"J", "N", 1, columns_-1, {{0, -1}, {0, 0}, {0, 1}, {1, 1}}};
-    piece_positions_[1] = {"J", "S", 1, columns_-1, {{0, -1}, {0, 0}, {0, 1}, {-1, -1}}};
-    piece_positions_[2] = {"J", "E", 1, columns_, {{-1, 0}, {0, 0}, {1, 0}, {1, -1}}};
-    piece_positions_[3] = {"J", "W", 0, columns_-1, {{-1, 0}, {0, 0}, {1, 0}, {-1, 1}}};
-    piece_positions_[4] = {"L", "N", 1, columns_-1, {{0, -1}, {0, 0}, {0, 1}, {1, -1}}};
-    piece_positions_[5] = {"L", "S", 1, columns_-1, {{0, -1}, {0, 0}, {0, 1}, {-1, 1}}};
-    piece_positions_[6] = {"L", "E", 1, columns_, {{-1, 0}, {0, 0}, {1, 0}, {-1, -1}}};
-    piece_positions_[7] = {"L", "W", 0, columns_-1, {{-1, 0}, {0, 0}, {1, 0}, {1, 1}}};
-    piece_positions_[8] = {"T", "N", 1, columns_-1, {{0, -1}, {0, 0}, {0, 1}, {1, 0}}};
-    piece_positions_[9] = {"T", "S", 1, columns_-1, {{0, -1}, {0, 0}, {0, 1}, {-1, 0}}};
-    piece_positions_[10] = {"T", "E", 1, columns_, {{0, -1}, {0, 0}, {-1, 0}, {1, 0}}};
-    piece_positions_[11] = {"T", "W", 0, columns_-1, {{0, 1}, {0, 0}, {-1, 0}, {1, 0}}};
-    piece_positions_[12] = {"O", "S", 0, columns_-1, {{0, 0}, {0, 1}, {1, 0}, {1, 1}}};
-    piece_positions_[13] = {"I", "S", 1, columns_-2, {{0, 0}, {0, 1}, {0, -1}, {0, 2}}};
-    piece_positions_[14] = {"I", "W", 0, columns_, {{0, 0}, {-1, 0}, {1, 0}, {2, 0}}};
-    piece_positions_[15] = {"S", "S", 0, columns_-1, {{0, 0}, {-1, 0}, {0, 1}, {1, 1}}};
-    piece_positions_[16] = {"S", "W", 1, columns_-1, {{0, -1}, {0, 0}, {-1, 0}, {-1, 1}}};
-    piece_positions_[17] = {"Z", "S", 0, columns_-1, {{0, 0}, {0, 1}, {-1, 1}, {1, 0}}};
-    piece_positions_[18] = {"Z", "W", 1, columns_-1, {{-1, -1}, {-1, 0}, {0, 0}, {0, 1}}};
+    piece_positions_[ 0] = {"J", "N", 0, columns_-2};
+    piece_positions_[ 1] = {"J", "S", 0, columns_-2};
+    piece_positions_[ 2] = {"J", "E", 0, columns_-1};
+    piece_positions_[ 3] = {"J", "W", 0, columns_-1};
+    piece_positions_[ 4] = {"L", "N", 0, columns_-2};
+    piece_positions_[ 5] = {"L", "S", 0, columns_-2};
+    piece_positions_[ 6] = {"L", "E", 0, columns_-1};
+    piece_positions_[ 7] = {"L", "W", 0, columns_-1};
+    piece_positions_[ 8] = {"T", "N", 0, columns_-2};
+    piece_positions_[ 9] = {"T", "S", 0, columns_-2};
+    piece_positions_[10] = {"T", "E", 0, columns_-1};
+    piece_positions_[11] = {"T", "W", 0, columns_-1};
+    piece_positions_[12] = {"O", "S", 0, columns_-1};
+    piece_positions_[13] = {"I", "S", 0, columns_-3};
+    piece_positions_[14] = {"I", "W", 0, columns_-0};
+    piece_positions_[15] = {"S", "S", 0, columns_-2};
+    piece_positions_[16] = {"S", "W", 0, columns_-1};
+    piece_positions_[17] = {"Z", "S", 0, columns_-1};
+    piece_positions_[18] = {"Z", "W", 0, columns_-1};
+
+    vector<vector<int>> positions[] = {
+      {{0, 0}, {0, 1}, {0, 2}, {1, 2}},
+      {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
+      {{2, 0}, {1, 1}, {2, 1}, {0, 1}},
+      {{0, 0}, {1, 0}, {2, 0}, {0, 1}},
+      {{0, 0}, {0, 1}, {0, 2}, {1, 0}},
+      {{1, 0}, {1, 1}, {1, 2}, {-1, 1}},
+      {{-1, 0}, {0, 0}, {1, 0}, {-1, -1}},
+      {{-1, 0}, {0, 0}, {1, 0}, {1, 1}},
+      {{0, -1}, {0, 0}, {0, 1}, {1, 0}},
+      {{0, -1}, {0, 0}, {0, 1}, {-1, 0}},
+      {{0, -1}, {0, 0}, {-1, 0}, {1, 0}},
+      {{0, 1}, {0, 0}, {-1, 0}, {1, 0}},
+      {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+      {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+      {{0, 0}, {-1, 0}, {1, 0}, {2, 0}},
+      {{0, 0}, {-1, 0}, {0, 1}, {1, 1}},
+      {{0, -1}, {0, 0}, {-1, 0}, {-1, 1}},
+      {{0, 0}, {0, 1}, {-1, 1}, {1, 0}},
+      {{-1, -1}, {-1, 0}, {0, 0}, {0, 1}},
+    };
 
     for (int i = 0; i < 19; ++i) {
-      PiecePositions piPos = piece_positions_[i];
-      for (int pos = piPos.min_x_; pos < piPos.max_x_; ++pos) {
+      PiecePositions& pp = piece_positions_[i];
+      vector<vector<int>>& p = positions[i];
+      pp.setPiecePosition(p);
+      pp.idx_ = piece_name_to_idx_[pp.name_];
+    }
+
+    for (int i = 0; i < 19; ++i) {
+      PiecePositions& pp = piece_positions_[i];
+      for (int pos_place = pp.min_x_; pos_place < pp.max_x_; ++pos_place) {
         AbsStartPiecePositions absPos;
-        absPos.copyPos(piPos.pos_);
+        absPos.copyPos(pp);
+
         for (int j = 0; j < 4; ++j) {
           absPos.pos_[j][0] += 1;
-          absPos.pos_[j][1] += pos;
+          absPos.pos_[j][1] += pos_place;
         }
-        // absPos.printPos();
-        key_name_dir_pos key = std::make_tuple(piPos.name_, piPos.direction_, pos);
+
+        key_name_dir_pos key = std::make_tuple(pp.name_, pp.direction_, pos_place);
         keys_.push_back(key);
         piece_name_direction_pos_to_abs_pos_[key] = absPos;
       }
@@ -158,6 +263,12 @@ public:
   ~TetrisField() {
     delete[] field_;
     delete[] field_arr_;
+  }
+
+  void printCurrentPiece() {
+    cout << "current_piece_.name_: " << current_piece_.name_ << endl;
+    cout << "current_piece_.direction_: " << current_piece_.direction_ << endl;
+    cout << "current_piece_.pos_place_: " << current_piece_.pos_place_ << endl;
   }
 
   uint8_t** const getField() {
@@ -186,25 +297,68 @@ public:
     return os;
   }
 
-  void setPiece(string piece_name, string direction, int pos) {
-    auto it = piece_name_direction_pos_to_abs_pos_.find(std::make_tuple(piece_name, direction, pos));
+  void setPiece(string piece_name, string direction, int pos_place) {
+    auto key = std::make_tuple(piece_name, direction, pos_place);
+    auto it = piece_name_direction_pos_to_abs_pos_.find(key);
     if(it != piece_name_direction_pos_to_abs_pos_.end())
     {
       auto absPos = it->second;
-      // absPos.printPos();
 
-      auto it_idx = piece_name_to_idx_.find(piece_name);
-      auto pcs_idx = it_idx->second;
-      auto pos = absPos.pos_;
-      for (int i = 0; i < 4; ++i) {
-        field_[pos[i][0]][pos[i][1]] = pcs_idx;
-      }
+      current_piece_.is_piece_set_ = true;
+      current_piece_.name_ = piece_name;
+      current_piece_.direction_ = direction;
+      current_piece_.key_ = key;
+      current_piece_.pos_place_ = pos_place;
+      current_piece_.idx_ = piece_name_to_idx_[piece_name];
+
+      current_piece_.copyPos(absPos);
     }
   }
 
   void setRandomPiece() {
     key_name_dir_pos k = *select_randomly(keys_.begin(), keys_.end());
     setPiece(std::get<0>(k), std::get<1>(k), std::get<2>(k));
+  }
+
+  bool moveCurrentPieceInstant() {
+    if (!current_piece_.is_piece_set_) {
+      return false;
+    }
+
+    const size_t size = current_piece_.pos_.size();
+
+    for (size_t j = 0; j < size; ++j) {
+      vector<int>& point = current_piece_.pos_[j];
+      if (field_[point[0]][point[1]] != 0) {
+        return false;
+      }
+    }
+
+    bool is_possible_to_move = true;
+    while (is_possible_to_move) {
+      for (size_t j = 0; j < size; ++j) {
+        vector<int>& point = current_piece_.pos_[j];
+        point[0] += 1;
+        if ((point[0] >= rows_) || (field_[point[0]][point[1]] != 0)) {
+          is_possible_to_move = false;
+          
+          for (int i = 0; i <= j; ++i) {
+            vector<int>& point_2 = current_piece_.pos_[i];
+            point_2[0] -= 1;
+          }
+          break;
+        }
+      }
+    }
+
+    for (size_t j = 0; j < size; ++j) {
+      vector<int>& point = current_piece_.pos_[j];
+      field_[point[0]][point[1]] = current_piece_.idx_;
+    }
+
+    current_piece_.is_piece_set_ = false;
+
+    return true;
   }
 
   void clearField() {
@@ -220,10 +374,13 @@ public:
 int main(int argc, char* argv[]) {
   TetrisField tf = TetrisField(20, 5);
 
-  while (std::cin.get() == '\n') {
+  do {
     tf.setRandomPiece();
+    bool isPieceMoved = tf.moveCurrentPieceInstant();
     cout << "field:" << endl << tf;
+    tf.printCurrentPiece();
     tf.clearField();
-  }
+    cout << "ENTER..." << endl;
+  } while (std::cin.get() == '\n');
   return 0;
 }
