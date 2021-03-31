275ae3d99530ddb21846216272b14cf51ac7545b
Hearues Zueke <hziko314@gmail.com>
2021-03-31 13:27:34 +0200

    Fixed some more little stuff

--- c_programs/test_static_compiling/write_objdump.sh
+++ c_programs/test_static_compiling/write_objdump.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+make all
+
 mkdir -p objdumps
+
+objdump -d objs/main_empty.o > objdumps/objdump_main_empty.txt
+objdump -d objs/main_include_stdio.o > objdumps/objdump_main_include_stdio.txt
 
 objdump -d objs/main_without_simple_library.o > objdumps/objdump_main_without_simple_library.txt
 objdump -d objs/main_with_simple_library.o > objdumps/objdump_main_with_simple_library.txt
