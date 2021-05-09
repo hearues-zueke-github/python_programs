eab46f33966c20697787f425ed79429a4d0f71b1
Hearues Zueke <hziko314@gmail.com>
2021-05-09 11:16:30 +0200

    changed the template ultra file

--- template_file_ultra.py
+++ template_file_ultra.py
@@ -14,28 +14,33 @@ import traceback
 
 import numpy as np
 import pandas as pd
+import multiprocessing as mp
 
 from copy import deepcopy, copy
 from dotmap import DotMap
 from functools import reduce
 from memory_tempfile import MemoryTempfile
 from shutil import copyfile
-
 from pprint import pprint
-from typing import List, Set, Tuple, Dict, Union
 
-from PIL import Image
+PATH_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))+"/"
+HOME_DIR = os.path.expanduser("~")+"/"
+TEMP_DIR = MemoryTempfile().gettempdir()+"/"
 
 import importlib.util as imp_util
-spec = imp_util.spec_from_file_location("utils", "../utils.py")
+
+# TODO: change the optaining of the git root folder from a config file first!
+spec = imp_util.spec_from_file_location("utils", os.path.join(HOME_DIR, "git/python_programs/utils.py"))
 utils = imp_util.module_from_spec(spec)
 spec.loader.exec_module(utils)
 
 mkdirs = utils.mkdirs
 
-PATH_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))+"/"
-HOME_DIR = os.path.expanduser("~")+"/"
-TEMP_DIR = MemoryTempfile().gettempdir()+"/"
+spec = imp_util.spec_from_file_location("utils_multiprocessing_manager", os.path.join(HOME_DIR, "git/python_programs/utils_multiprocessing_manager.py"))
+utils_multiprocessing_manager = imp_util.module_from_spec(spec)
+spec.loader.exec_module(utils_multiprocessing_manager)
+
+MultiprocessingManager = utils_multiprocessing_manager.MultiprocessingManager
 
 OBJS_DIR_PATH = PATH_ROOT_DIR+'objs/'
 mkdirs(OBJS_DIR_PATH)
