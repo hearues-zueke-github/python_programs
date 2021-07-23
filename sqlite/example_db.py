c6696078960ef5d2dd59a1f5960543115367b316
Hearues Zueke <hziko314@gmail.com>
2021-07-23 19:47:22 +0200

    added more functions for sqlite

--- sqlite/example_db.py
+++ sqlite/example_db.py
@@ -1,8 +1,13 @@
-#! /usr/bin/python3.6
+#! /usr/bin/python3.9.5
 
+import os
 import sqlite3
 
 import numpy as np
+import pandas as pd
+
+# own
+import constants
 
 def do_table_name_exists(conn, table_name):
     cur = conn.cursor()
@@ -24,20 +29,46 @@ def create_table_colors(conn):
         cur.execute('INSERT INTO colors (idx, hex1) VALUES (?,?)', (i, hex_str))
 
     conn.commit()
-
-
-def get_whole_table(conn, table_name):
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM {}'.format(table_name))
-    l = [v for v in cur]
-    return l
     
 
-if __name__=='__main__':
-    print("Hello World!")
-
-    conn = sqlite3.connect('company.sqlite')
+def get_df(conn, sql_query):
     cur = conn.cursor()
+    cur.execute(sql_query)
+    l_column = [x[0] for x in cur.description]
+    l_data = cur.fetchall()
+    df = pd.DataFrame(data=l_data, columns=l_column, dtype=object)
+    
+    return df
+
+
+def get_d_df(conn):
+    d_df = {}
+
+    sql_query_template = "SELECT * FROM {table_name}"
+
+    print("Getting 'sqlite_master' Table.")
+    df_master = get_df(conn=conn, sql_query=sql_query_template.format(table_name='sqlite_master'))
+    d_df['sqlite_master'] = df_master
+    globals()['df_master'] = df_master
+
+    for name, v_type in df_master[['name', 'type']].values:
+        if v_type != 'table':
+            continue
+        
+        print("Getting '{name}' Table.".format(name=name))
+        df_master = get_df(conn=conn, sql_query=sql_query_template.format(table_name=name))
+        d_df[name] = df_master
+
+    return d_df
+
+
+if __name__=='__main__':
+    conn = sqlite3.connect(os.path.join(constants.file_path_sqlite_db))
+    
+    d_df = get_d_df(conn=conn)
+
+    # conn = sqlite3.connect('company.sqlite')
+    # cur = conn.cursor()
     # cur.execute('DROP TABLE IF EXISTS employee')
     # cur.execute('CREATE TABLE employee (empid INT, ename TEXT, designation TEXT) ')
     # cur.execute('INSERT INTO employee (empid, ename, designation) VALUES (?,?,?)', (210, 'Ahmed', 'Instructor' ))
