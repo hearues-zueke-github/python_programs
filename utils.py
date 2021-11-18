d18c42c55a67f528b96d408e642b81897ae1b35a
Hearues Zueke <hziko314@gmail.com>
2021-11-18 09:16:17 +0100

    small changes

--- utils.py
+++ utils.py
@@ -5,3 +5,6 @@ from typing import List, Dict, Set, Mapping, Any, Union
 def mkdirs(path : str) -> None:
     if not os.path.exists(path):
         os.makedirs(path)
+
+def path_exists(path: str) -> bool:
+    return os.path.exists(path)
